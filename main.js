c8af40e35ba60be339d1ec3ecf31641c4054e6a1
Paul Sawaya <me@paulsawaya.com>
2012-10-27 20:02:03 -0700

    Simple express app, with bootstrap and jquery

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.configure(function () {
+    // Serve static files
+    app.use('/static',express.static('static'));
+});
+
 app.get('/', function(req, res){
   res.send('games \'n glpyhs!');
+});
+
+app.get('/author', function(req, res) {
+    res.render('author');
 });
 
 app.listen(3000);
